32b34ac8cf0063a2400f68c4d771209f9a8846f9
Andrew Hundt <ATHundt@gmail.com>
2025-11-13 21:30:51 +0000

    Remove _site build artifacts from git tracking

--- _site/CalcHundter/Contact_files/Contact.js
+++
@@ -1,9 +0,0 @@
-// Created by iWeb 3.0.4 local-build-20131216
-
-setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
-{return false;}
-function onPageLoad()
-{loadMozillaCSS('Contact_files/ContactMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
-function onPageUnload()
-{Widget.onunload();}
